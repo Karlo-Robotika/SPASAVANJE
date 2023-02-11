9c8110b9d27b9cd9763def93b4b7aebe2d2add3c
Karlo-Robotika <115877466+Karlo-Robotika@users.noreply.github.com>
2023-02-11 13:53:59 +0100

    Update .cpp

--- FUNKCIJE/.cpp
+++ FUNKCIJE/.cpp
@@ -1441,7 +1441,42 @@ void RobotLine::Frontwalls90() {
 }
 
 
+ void RobotLine::nigerija() {
+    
+  if (line(8))
+     go(-90, 90);
+   else if (line(0))
+    go(90, -90);
+   else if (line(7))
+    go(0, 100);
+   else if (line(1))
+    go(100, 0);
+   else if (line(6))
+    go(30, 80);
+   else if (line(2))
+    go(80, 30);
+   else if (line(5))
+    go(40, 70);
+   else if (line(3))
+    go(70, 40);  
+   else if (line(4))
+    go(70, 70);
+   else
+    go(70, 70);
+  }
 
+void RobotLine::deez() {
+ wallsFollowLeft();
+ if (frontRight() < 120) {
+  go(90, -90);
+  delayMs(500);
+  go(60, 60);
+  delayMs(500);
+  armClose();
+
+  }
+
+  }
 
 
 
