b726e7c41ac65c20478b5c09ab02e29ff01b57ba
Karlo-Robotika <115877466+Karlo-Robotika@users.noreply.github.com>
2023-03-04 13:55:17 +0100

    Update radionice.h

--- FUNKCIJE/radionice.h
+++ FUNKCIJE/radionice.h
@@ -6,14 +6,32 @@ void RobotLine::radionica()
 {
 nigerija();
 
- if (line(0) && line(8)) {
+
+if (line(0) && line(8) &&   pitch() < -10) {
+    go(90, 90);
+    delayMs(200);
+
+  }
+
+
+  
+else if (line(0) && line(8) &&   pitch() < 10 && pitch()>2) {
+    go(90, 90);
+    delayMs(200);
+
+  }
+  
+else if (line(0) && line(8)) {
     go(-90, 90);
     delayMs(500);
 
   }
 
  
-  if (front() < 120) {
+
+
+ 
+    if (front() < 120 and front()>50) {
     go(-90, 90);
     delayMs(500);
     go(90, -90);
@@ -27,6 +45,6 @@ nigerija();
     go(-90, 90);
     delayMs(500);
 
-  }
+  } 
 
 }
