50efd340e145716470d0226b91e2601a497afc33
Karlo-Robotika <115877466+Karlo-Robotika@users.noreply.github.com>
2023-04-22 13:48:29 +0200

    Update radionice.h

--- FUNKCIJE/radionice.h
+++ FUNKCIJE/radionice.h
@@ -9,28 +9,8 @@ static int dua=0;
 nigerija();
 
 
-if (line(0) && line(8) &&   pitch() < -10) {
-    go(90, 90);
-    delayMs(200);
-    dua=1;
 
-  }
-
-
-  
-else if (line(0) && line(8) &&   pitch() < 10 && pitch()>2) {
-    go(50, 50);
-    delayMs(300);
-
-  }
-  
-else if (line(0) && line(8) && dua==0) {
-    go(-90, 90);
-    delayMs(500);
-
-  }
-
-if(front(3)<120 and front(3)>50 and (pitch()>-2 and pitch()<2) and shrek == 0) {
+if(front(3)<120 and front(3)>50 and (pitch()>-2 and pitch()<2) and shrek == 0 && dua==0) {
     go(-90, 90);
     delayMs(500);
     
@@ -46,5 +26,33 @@ if(front(3)<120 and front(3)>50 and (pitch()>-2 and pitch()<2) and shrek == 0) {
  delayMs(500);
  shrek++;
 }
+
+if((shrek==1) and (dua==1) and not lineAny() and frontLeft()<330 and frontRight()<330 and pitch()>-2 and pitch()<2){
+  stop();
+  end();
+  
+}
+
+if (line(0) && line(8) &&   pitch() < -10 and not shrek == 0) {
+    go(90, 90);
+    delayMs(200);
+    dua=1;
+
+  }
+
+
+  
+else if (line(0) && line(8) &&   pitch() < 10 && pitch()>2) {
+    go(50, 50);
+    delayMs(300);
+    armOpen();
+
+  }
+  
+else if (line(0) && line(8) && dua==0) {
+    go(-90, 90);
+    delayMs(500);
+
+  }
 
 }
